000574feee91fe0279fc93bcd9cab71e8019e351
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-11 21:21:22 -0500

    Fixed player 1 and player 2 scoring issues

--- Strings/src/com/fad/ifkeycodeblock.java
+++ Strings/src/com/fad/ifkeycodeblock.java
@@ -4,12 +4,14 @@ public class ifkeycodeblock {
 
     public static void main(String[] args){
         boolean gameOver = true;
-        int p1Score = 500;
+        int p1Score = 201;
         int p2Score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-        int p1FinalScore = (p1Score+bonus*levelCompleted);
-        int p2FinalScore = (p2Score + bonus * levelCompleted);
+        int p1levelCompleted = 5;
+        int p2levelCompleted = 8;
+        int p2Bonus = 200;
+        int p1Bonus = 100;
+        int p1FinalScore = (p1Score + p1Bonus * p1levelCompleted);
+        int p2FinalScore = (p2Score + p2Bonus * p2levelCompleted);
 
 
         if(p1FinalScore < 5000 && p1FinalScore > 1001) {
@@ -21,11 +23,11 @@ public class ifkeycodeblock {
         }
 
         if(p2FinalScore < 5000 && p2FinalScore > 1001) {
-            System.out.println("Player 2's Score was " + p1FinalScore + " which is less than 5000 BUT greater than 1000");
+            System.out.println("Player 2's Score was " + p2FinalScore + " which is less than 5000 BUT greater than 1000");
         } else if (p1FinalScore < 1000) {
-            System.out.println("Player 2's score was " + p1FinalScore + " which is a Terrible score");
+            System.out.println("Player 2's score was " + p2FinalScore + " which is a Terrible score");
         } else {
-            System.out.println("Player 2's score was " + p1FinalScore + " which means you've won!");
+            System.out.println("Player 2's score was " + p2FinalScore + " which means you've won!");
         }
 
         if(p1FinalScore > p2FinalScore) {
