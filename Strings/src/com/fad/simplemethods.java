8e55e1a7d794ca20d5955556ca46e3aeffe2d8af
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-12 22:14:45 -0500

    Created calculateScore method

--- Strings/src/com/fad/simplemethods.java
+++ Strings/src/com/fad/simplemethods.java
@@ -8,11 +8,7 @@ public class simplemethods {
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(true, 800, 4, 100);
 
         score = 10000;
         levelCompleted = 8;
@@ -24,11 +20,7 @@ public class simplemethods {
         }
     }
 
-    public static void calculateScore() {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
