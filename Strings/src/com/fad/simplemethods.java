d90ef0d37c7319e4c596b6bc1ff313403f6b26b3
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-13 21:43:07 -0500

    Added boolean for game not over

--- Strings/src/com/fad/simplemethods.java
+++ Strings/src/com/fad/simplemethods.java
@@ -4,20 +4,24 @@ public class simplemethods {
 
     public static void main(String [] args) {
 
-        calculateScore(true, 800, 5, 175);
+        calculateScore(false, 800, 5, 175);
 
         calculateScore(true, 800, 4, 100);
 
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             System.out.println("Your final score was" + finalScore);
+            return finalScore;
+        }
+
+        else {
+            return -1;
         }
     }
-
 
 }
