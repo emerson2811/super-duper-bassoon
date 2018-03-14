0b76fc53fc45e0f57f5eef2428af4df92df49e6e
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-13 21:21:20 -0500

    improved the scoring method.

--- Strings/src/com/fad/simplemethods.java
+++ Strings/src/com/fad/simplemethods.java
@@ -3,28 +3,18 @@ package com.fad;
 public class simplemethods {
 
     public static void main(String [] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+
+        calculateScore(true, 800, 5, 175);
 
         calculateScore(true, 800, 4, 100);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was" + finalScore);
         }
     }
