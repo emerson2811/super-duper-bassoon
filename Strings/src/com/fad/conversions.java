cc0eae0c9289c6922bf593720cf7bd3e555bf3d1
emerson2811 <adam.emerson@sanfordhealth.org>
2018-03-15 21:12:24 -0500

    Redid the conversion class

--- Strings/src/com/fad/conversions.java
+++ Strings/src/com/fad/conversions.java
@@ -4,11 +4,26 @@ public class conversions {
 
     public static void main(String [] args) {
         int feetToInches = (measurement(12, 5) * 12);
+        calcFeetAndInchesToCentimeters(-12, 5);
 
     }
 
     public static int measurement (int feet, int inches) {
         System.out.println(feet + " feet equals " + inches + " inches!");
         return inches;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+
+        if ((feet < 0) || ((inches < 0 ) || (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters );
+        return centimeters;
+
     }
 }
